2cfe6e093a2806d1bf9defe79f08b91a30019d09
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-08-31 14:59:21 -0300

    deprecate -esnipe

---
+++ Commands/Fun/esnipe.js
@@ -0,0 +1,25 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: 'esnipe',
+    category: 'Fun',
+    description: 'Returns the last edited message in current or specified channel',
+    aliases: ['editsnipe'],
+    syntax: `esnipe [channel]`,
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {string[]} args 
+     * @param {String} prefix 
+     */
+    run: (client, message, args, prefix) => {
+        const embed = new EmbedBuilder()
+        .setTitle(`💀 ~~i saw what you edited~~`)
+        .setColor(0xFFFF00)
+        .setDescription(`:calmcryandsmokin: This command is no longer available.`)
+        .setFooter({text: `Note: -esnipe will be removed soon.`})
+
+        message.channel.send({embeds: [embed]})
+    }
+}
