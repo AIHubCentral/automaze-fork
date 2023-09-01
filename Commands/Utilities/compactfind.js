ed658fa1c6cfb42195a804a8cffa3c84b53175f7
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-08-31 21:48:36 -0300

    tell user to visit the voice-models channel when using -find and -cfind

--- Commands/Utilities/compactfind.js
+++ Commands/Utilities/compactfind.js
@@ -14,52 +14,15 @@ module.exports = {
      * @param {String} prefix 
      */
     run: async (client, message, args, prefix) => {
-        if (client.disallowedChannelIds.includes(`${message.channelId}`)) {
-            if (message.content.startsWith('-cfind')) {
-                const botReply = await message.reply('This command is not available in this channel.');
+        // which channel send to user
+        const targetChannel = message.guild.channels.cache.get(client.discordIDs.Forum.VoiceModel);
+        const botReply = await message.reply(`This command is currently unavailable. Please visit the ${targetChannel} channel if you wish to search for a model.`);
 
-                setTimeout(async () => {
-                    await message.delete();
-                    await botReply.delete();
-                }, 6000);
-
-                return;
-            }
-        }
-
-        const query = message.content.split(' ').slice(1).join(' ');
-
-        if (!query) {
-            return void message.reply(`no query provided`);
-        }
-
-        let allResults = client.modelSearchEngine.search(query, { fuzzy: 0.2 }).filter(result => result.downloadURL && result.downloadURL.length);
-
-        allResults.sort((a, b) => b.score - a.score); // GPT-generated code, sort the results in descending order
-
-        let results;
-        let resultsLeft;
-
-        if (!allResults.length) {
-            return void message.channel.send({ embeds: [new EmbedBuilder().setTitle('No result found.').setColor(`Red`)] });
-        }
-
-        if (allResults.length > 3) {
-            results = allResults.slice(0, 3);
-            resultsLeft = allResults.length - 3;
-        } else {
-            results = allResults;
-        }
-
-        const resultEmbed = new EmbedBuilder()
-            .setTitle(`${allResults.length} results found - Search mode: R-D, fuzzy: 0.2, compact`)
-            .setDescription(results.map(result => `- [${result.title}](${result.downloadURL[0]}) ${result.tags.map(tag => `${tag ? tag.icon : `Deleted Icon`}`).join(``)} - ${result.creator}`).join(`\n`))
-            .setColor(`Green`);
-
-        if (resultsLeft) {
-            resultEmbed.setFooter({ text: `And ${resultsLeft} more results...` })
-        }
-
-        message.channel.send({ embeds: [resultEmbed] });
+        // delete the messages after 30 seconds
+        const messageTimeoutSeconds = 30;
+        setTimeout(async() => {
+            await message.delete();
+            await botReply.delete();
+        }, messageTimeoutSeconds * 1000);
     }
 }
