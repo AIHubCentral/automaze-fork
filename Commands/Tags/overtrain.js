3d83f134d2fb2ec432da8e10818d0a12e834d942
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-09-06 09:05:58 -0300

    add overtraining detector link in overtrain.js

--- Commands/Tags/overtrain.js
+++ Commands/Tags/overtrain.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'overtrain',
     category: 'Tags',
     description: 'How to tell whether your model is overtraining and what to do',
-    aliases: [],
+    aliases: ['overtraining'],
     syntax: `overtrain [member]`,
     /**
      * 
@@ -15,9 +15,10 @@ module.exports = {
      */
     run: (client, message, args, prefix) => {
         const embed = new EmbedBuilder()
-        .setTitle(`Overtrain?`)
-        .setImage(`https://media.discordapp.net/attachments/1124354352124010536/1126132264569417728/image.png`)
-        .setColor(`Yellow`);
+            .setTitle(`How do I know if my model is overtaining?`)
+            .setImage(`https://media.discordapp.net/attachments/1124354352124010536/1126132264569417728/image.png`)
+            .setDescription('## Realtime Overtraining Detection (ROD)\nhttps://github.com/grvyscale/RealtimeOvertrainingDetection\n\nClick on the image below to learn more about overtraining')
+            .setColor(`Yellow`);
 
         if (message.mentions.members.first()) {
             return void message.channel.send({content: `*Tag suggestion for ${message.mentions.members.first()}*`, embeds: [embed]});
