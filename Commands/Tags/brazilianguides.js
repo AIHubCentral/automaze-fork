2c306f76d1cfa78f916adbae72b183d8475c38b7
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-09-06 10:09:45 -0300

    add real time guide in brazilian guides

--- Commands/Tags/brazilianguides.js
+++ Commands/Tags/brazilianguides.js
@@ -5,7 +5,7 @@ module.exports = {
     category: 'Tags',
     description: 'Guias traduzidos em português para iniciantes.',
     aliases: ['br_guides', 'br_g'],
-    syntax: `brazilianguides`,
+    syntax: `br_g [member]`,
     /**
      * 
      * @param {Client} client 
@@ -14,10 +14,21 @@ module.exports = {
      * @param {String} prefix 
      */
     run: async (client, message, args, prefix) => {
+        const embedDescription = [];
+        embedDescription.push('## • Criando covers e treino utilizando Colab: \nhttps://docs.google.com/document/d/1Mmoocy4luVSFRqqGI1NGQHbMGKMNRPNyFrhJRuLEkcs/?usp=sharing');
+        embedDescription.push('## • Instalando RVC Localmente:\nhttps://docs.google.com/document/d/1mivBJRFtcg4erID_9jfxJA_ycXRGUmiCm37CuF8xVA0');
+        embedDescription.push('## • Como usar o RVC em tempo real (por Rafa.Godoy)\nhttps://docs.google.com/document/d/1Do49gmj7nKj35sZc-5WHXtwZHg0-cSPnH--6WdlvPSc/edit?usp=sharing');
+        embedDescription.push('\n### Servidor **AI HUB Brasil**\nhttps://discord.gg/aihubbrasil')
+
         const embed = new EmbedBuilder()
-        .setTitle(`Guias Brasileiros`)
-        .setDescription(`## • Criando covers e treino utilizando Colab: \nhttps://docs.google.com/document/d/1Mmoocy4luVSFRqqGI1NGQHbMGKMNRPNyFrhJRuLEkcs/?usp=sharing\n## • Instalando RVC Localmente:\nhttps://docs.google.com/document/d/1mivBJRFtcg4erID_9jfxJA_ycXRGUmiCm37CuF8xVA0`)
-        .setColor(`Yellow`);
+            .setTitle(`Guias PT-BR`)
+            .setDescription(embedDescription.join('\n'))
+            .setColor(`Yellow`)
+            .setFooter({ text:'Créditos: MrM0dZ, Rafa.Godoy e Inconstantino' });
+
+        if (message.mentions.members.first()) {
+            return message.channel.send({content: `*Sugestão para ${message.mentions.members.first()}*`, embeds: [embed]});
+        }
 
         message.channel.send({embeds: [embed]});
     }
