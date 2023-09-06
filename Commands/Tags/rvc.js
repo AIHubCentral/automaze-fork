c24a7ac6d4c60d015915144e645adba042c006ed
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-09-06 09:51:40 -0300

    update links in -rvc command and add easter egg

--- Commands/Tags/rvc.js
+++ Commands/Tags/rvc.js
@@ -1,4 +1,5 @@
-const { EmbedBuilder } = require("discord.js")
+const { EmbedBuilder } = require("discord.js");
+const utils = require('../../utils.js');
 
 module.exports = {
     name: 'rvc',
@@ -14,13 +15,23 @@ module.exports = {
      * @param {String} prefix 
      */
     run: (client, message, args, prefix) => {
+        const embedDescription = [];
+        embedDescription.push(`## • RVC v2 AI Cover Guide (by Kalomaze)\nhttps://docs.google.com/document/d/13_l1bd1Osgz7qlAZn-zhklCbHpVRk6bYOuAuB78qmsE/edit?usp=sharing`);
+        embedDescription.push(`## • Training RVC v2 models guide (by Kalomaze)\nhttps://docs.google.com/document/d/13ebnzmeEBc6uzYCMt-QVFQk-whVrK4zw8k7_Lw3Bv_A/edit?usp=sharing`);
+        embedDescription.push(`### • Installing Mangio RVC v2 Locally (OUTDATED)\nhttps://docs.google.com/document/d/1KKKE7hoyGXMw-Lg0JWx16R8xz3OfxADjwEYJTqzDO1k/edit`);
+
         const embed = new EmbedBuilder()
-        .setTitle(`RVC Guides`)
-        .setDescription(`## • Make an AI cover using an existing model on RVC v2\nhttps://docs.google.com/document/d/13_l1bd1Osgz7qlAZn-zhklCbHpVRk6bYOuAuB78qmsE/edit\n## • Train a new voice model using RVC v2\nhttps://docs.google.com/document/d/13ebnzmeEBc6uzYCMt-QVFQk-whVrK4zw8k7_Lw3Bv_A/edit\n## • Install RVC v2 locally\nhttps://docs.google.com/document/d/1KKKE7hoyGXMw-Lg0JWx16R8xz3OfxADjwEYJTqzDO1k/edit`)
-        .setColor(`Yellow`);
+            .setTitle(`RVC Guides (how to make ai cover)`)
+            .setDescription(embedDescription.join('\n'))
+            .setColor(`Yellow`);
 
         if (message.mentions.members.first()) {
-            return void message.channel.send({content: `*Tag suggestion for ${message.mentions.members.first()}*`, embeds: [embed]});
+            // easter egg - show 'how to make ai cover meme'
+            const randomNumber = utils.getRandomNumber(1, 100);
+            if (randomNumber === 50) {
+                embed.setImage('https://cdn.discordapp.com/attachments/1144111245415420017/1144111245709025390/7q6fht.jpg');
+            }
+            return message.channel.send({content: `*Tag suggestion for ${message.mentions.members.first()}*`, embeds: [embed]});
         }
 
         message.channel.send({embeds: [embed]});
