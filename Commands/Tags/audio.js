8ce6ef295e2c71939de7ef3cf612381a050654b4
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-09-06 07:30:42 -0300

    rename dataset.js to audio.js

--- Commands/Tags/audio.js
+++ Commands/Tags/audio.js
@@ -1,11 +1,11 @@
 const { EmbedBuilder } = require("discord.js")
 
 module.exports = {
-    name: 'dataset',
+    name: 'audio',
     category: 'Tags',
-    description: 'Guides on how to make datasets for training RVC models',
-    aliases: [],
-    syntax: `dataset [member]`,
+    description: 'Guides on how to isolate audio for making datasets',
+    aliases: ['dataset'],
+    syntax: `audio [member]`,
     /**
      * 
      * @param {Client} client 
@@ -16,10 +16,10 @@ module.exports = {
     run: (client, message, args, prefix) => {
         const embedDescription = [];
         embedDescription.push(`## • Perfecting Audio Isolation on Low-End Rigs: A Practical Guide\nhttps://docs.google.com/document/d/1HmkG9cmL8SLX7-vJcPT1-1KgUQtCrwXB8CicYmG4LW8/edit?usp=sharing`);
-        embedDescription.push(`## • Guide: Gathering and Isolating Audio (by SCRFilms)\nhttps://docs.google.com/document/d/1wTJ_wutDqEtsA99BJOXDDGax25pPIDE84O5E2Rio5Qk/edit#heading=h.g896c5vbh0dv`);
+        embedDescription.push(`## • Guide: Gathering and Isolating Audio\nhttps://docs.google.com/document/d/1wTJ_wutDqEtsA99BJOXDDGax25pPIDE84O5E2Rio5Qk/edit#heading=h.g896c5vbh0dv`);
 
         const embed = new EmbedBuilder()
-            .setTitle(`Guides on how to create a good Dataset`)
+            .setTitle(`Guides on how to isolate audio to create a good Dataset`)
             .setDescription(embedDescription.join('\n'))
             .setColor(`Yellow`)
             .setFooter({text:'Credits: Litsa The Dancer, Faze Masta and SCRFilms'});
