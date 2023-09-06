bb893f09fb189591b21cd6a3fafd02b885c3e78f
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-09-06 08:58:38 -0300

    update google colab links to include applio

--- Commands/Tags/audio.js
+++ Commands/Tags/audio.js
@@ -13,7 +13,7 @@ module.exports = {
      * @param {string[]} args 
      * @param {String} prefix 
      */
-    run: (client, message, args, prefix) => {
+    run: async(client, message, args, prefix) => {
         const embedDescription = [];
         embedDescription.push(`## • Perfecting Audio Isolation on Low-End Rigs: A Practical Guide\nhttps://docs.google.com/document/d/1HmkG9cmL8SLX7-vJcPT1-1KgUQtCrwXB8CicYmG4LW8/edit?usp=sharing`);
         embedDescription.push(`## • Guide: Gathering and Isolating Audio\nhttps://docs.google.com/document/d/1wTJ_wutDqEtsA99BJOXDDGax25pPIDE84O5E2Rio5Qk/edit#heading=h.g896c5vbh0dv`);
@@ -25,7 +25,7 @@ module.exports = {
             .setFooter({text:'Credits: Litsa The Dancer, Faze Masta and SCRFilms'});
 
         if (message.mentions.members.first()) {
-            return void message.channel.send({content: `*Tag suggestion for ${message.mentions.members.first()}*`, embeds: [embed]});
+            return message.channel.send({content: `*Tag suggestion for ${message.mentions.members.first()}*`, embeds: [embed]});
         }
 
         message.channel.send({embeds: [embed]});
--- Commands/Tags/colab.js
+++ Commands/Tags/colab.js
@@ -14,10 +14,14 @@ module.exports = {
      * @param {String} prefix 
      */
     run: (client, message, args, prefix) => {
+        const embedDescription = [];
+        embedDescription.push(`## • Applio Colab 🍏 (NEWEST)\nhttps://colab.research.google.com/drive/157pUQep6txJOYModYFqvz_5OJajeh7Ii`);
+        embedDescription.push(`### • RVC v2 Easy GUI Colab (outdated)\nhttps://colab.research.google.com/drive/1Gj6UTf2gicndUW_tVheVhTXIIYpFTYc7?usp=sharing`);
+
         const embed = new EmbedBuilder()
-        .setTitle(`RVC Colabs`)
-        .setDescription(`## • RVC v2 Easy GUI Colab\nhttps://colab.research.google.com/drive/1Gj6UTf2gicndUW_tVheVhTXIIYpFTYc7?usp=sharing\n## • RVC v2 Training Colab\nhttps://colab.research.google.com/drive/1TU-kkQWVf-PLO_hSa2QCMZS1XF5xVHqs?usp=sharing`)
-        .setColor(`Yellow`);
+            .setTitle(`Google Colabs`)
+            .setDescription(embedDescription.join('\n'))
+            .setColor(`Yellow`);
 
         if (message.mentions.members.first()) {
             return void message.channel.send({content: `*Tag suggestion for ${message.mentions.members.first()}*`, embeds: [embed]});
