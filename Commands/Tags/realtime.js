23cdaf7bc4a89dae5391c9b4ed347c04d2bc9a0c
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-09-22 21:20:34 -0300

    update realtime guide link to rentry instead of github

--- Commands/Tags/realtime.js
+++ Commands/Tags/realtime.js
@@ -15,7 +15,7 @@ module.exports = {
      */
     run: (client, message, args, prefix) => {
         const embedDescription = [];
-        embedDescription.push(`## 1. (Local) W-Okada Voice Changer\nThis has more options and is somewhat more convenient to use than Real-time RVC.\n**GUIDE**: https://github.com/YunaOneeChan/Voice-Changer-Settings`);
+        embedDescription.push(`## 1. (Local) W-Okada Voice Changer\nThis has more options and is somewhat more convenient to use than Real-time RVC.\n**GUIDE**: https://rentry.co/VoiceChangerGuide`);
         embedDescription.push(`## 2. (Online) W-Okada - Colab Version\nA free, online version of W-Okada that allows you to run code on Google’s powerful GPUs.\n**GUIDE**: https://docs.google.com/document/d/e/2PACX-1vTIceEcBfS6Zqolv_QEysrFfI_EJikPxozWptP_EjkpLVl-l-gdo-ijBonQMTviAHEYm5emmd9k9TdC/pub`);
         embedDescription.push(`## 3. (Local) Real-time conversion of RVC\nIt's more accurate but requires a Nvidia RTX card.\n**GUIDE**: https://docs.google.com/document/d/1haQAWn4Hnh3Aq8SSGX0tBSY3rDzjYJAcczrUy63oTTs/edit`);
         embedDescription.push('## 4. Russian FAQ for RVC/Voice changer\nhttps://github.com/MaHivka/ultimate-voice-models-FAQ');
