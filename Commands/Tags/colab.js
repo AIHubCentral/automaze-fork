b1faf90deb7e7bb82f4e5c489b156b9a24a4076a
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-09-30 21:26:20 -0300

    add ai cover gen colab link

--- Commands/Tags/colab.js
+++ Commands/Tags/colab.js
@@ -16,8 +16,10 @@ module.exports = {
     run: (client, message, args, prefix) => {
         const embedDescription = [];
         embedDescription.push('## RVC');
+        embedDescription.push('### • AI-Cover-Gen-No-UI\nhttps://colab.research.google.com/github/ardha27/AICoverGen-NoUI-Colab/blob/main/CoverGen_No_UI.ipynb');
+        embedDescription.push('> Note: This colab is for **inference** only.');
         embedDescription.push('### • RVC v2 Disconnected\nhttps://colab.research.google.com/drive/1XIPCP9ken63S7M6b5ui1b36Cs17sP-NS');
-        embedDescription.push('\n> Note: This colab is for training only, inference should be done locally due to new colab restrictions.');
+        embedDescription.push('> Note: This colab is for **training** only.');
         /*
         embedDescription.push(`### • Applio Colab 🍏 (NEWEST)\nhttps://colab.research.google.com/drive/157pUQep6txJOYModYFqvz_5OJajeh7Ii`);
         embedDescription.push(`### • RVC v2 Easy GUI Colab (outdated)\nhttps://colab.research.google.com/drive/1Gj6UTf2gicndUW_tVheVhTXIIYpFTYc7?usp=sharing`);
@@ -27,7 +29,7 @@ module.exports = {
         embedDescription.push('## Audio Separation/Isolation');
         embedDescription.push('### • MDX Net Colab\nhttps://colab.research.google.com/github/NaJeongMo/Colab-for-MDX_B/blob/main/MDX-Net_Colab.ipynb');
         embedDescription.push('### • UVR arch colab 5.0.2/c1.04\nhttps://colab.research.google.com/drive/16Q44VBJiIrXOgTINztVDVeb0XKhLKHwl');
-        embedDescription.push('### • 🆕 KaraFan colab\nhttps://colab.research.google.com/github/Captain-FLAM/KaraFan/blob/master/KaraFan.ipynb');
+        embedDescription.push('### • KaraFan colab\nhttps://colab.research.google.com/github/Captain-FLAM/KaraFan/blob/master/KaraFan.ipynb');
         
 
         const embed = new EmbedBuilder()
@@ -36,7 +38,7 @@ module.exports = {
             .setColor(`Yellow`);
 
         if (message.mentions.members.first()) {
-            return void message.channel.send({content: `*Tag suggestion for ${message.mentions.members.first()}*`, embeds: [embed]});
+            return void message.channel.send({content: `*Colab suggestions for ${message.mentions.members.first()}*`, embeds: [embed]});
         }
 
         message.channel.send({embeds: [embed]});
