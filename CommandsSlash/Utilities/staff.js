a0a523ccd5c6e32081caae243f27a790cc578430
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-10-01 19:15:11 -0300

    create command to list helpers, qc and mods

--- CommandsSlash/Utilities/staff.js
+++ CommandsSlash/Utilities/staff.js
@@ -7,6 +7,35 @@ module.exports = {
                 .setName('staff')
                 .setDescription('See staff activities'),
     async execute(interaction) {
-        await interaction.reply({content: 'test', ephemeral:true});
+        const client = interaction.client;
+        await interaction.deferReply({ ephemeral: true });
+        await interaction.guild.members.fetch();
+
+        const qcRole = await interaction.guild.roles.cache.find((role) => role.id == client.discordIDs.Roles.ModelQC);
+        const helperRole = await interaction.guild.roles.cache.find((role) => role.id == client.discordIDs.Roles.Helper);
+        const modRole = await interaction.guild.roles.cache.find((role) => role.id == client.discordIDs.Roles.Mod);
+        
+        const roles = [qcRole, helperRole, modRole];
+        let output = `Activity since ${new Date()}`;
+
+        for (let role of roles) {
+            let roleMembers = role.members;
+            roleMembers = roleMembers.filter((member) => !member.user.bot); // remove bots
+
+            let memberNames = roleMembers.map((member) => member.user.username);
+            output += `\n${role}`;
+            for (let name of memberNames) {
+                output += '\n- ' + name;
+            }
+            output += '\n';
+        }
+
+        const embed = new EmbedBuilder()
+            .setTitle('Staff Team')
+            .setColor('Blurple')
+            .setDescription(output)
+            .setTimestamp();
+
+        await interaction.editReply({embeds: [embed]});
     }
 }
