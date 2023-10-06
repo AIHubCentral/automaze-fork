098fe3d0a055b20928ae2d3be40b373749946b0e
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-10-06 00:17:15 -0300

    show the guilds the bot is in on startup

--- Events/ready.js
+++ Events/ready.js
@@ -3,5 +3,12 @@ module.exports = {
     once: true,
     run(client){
         console.log(`\nReady! Logged in as ${client.user.tag}`);
+
+        const guilds = client.guilds.cache;
+        console.log(`\nBot is currently in ${guilds.size} guild(s):`);
+
+        guilds.forEach((guild) => {
+            console.log(`${guild.id}: ${guild.name}`);
+        });
     }
 };
