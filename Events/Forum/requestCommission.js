2e5b5f0083b6480cf396a9db56616ee1728efe57
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-10-07 20:02:39 -0300

    update bot response to the comission thread

--- Events/Forum/requestCommission.js
+++ Events/Forum/requestCommission.js
@@ -24,20 +24,22 @@ module.exports = {
             }
             MessageRoles = MessageRoles.slice(0, -2) + ")";
 
+            const embedData = {
+                description: [
+                    `Hello! <@${Threader.ownerId}>`,
+                    'I\'ve noticed that you\'ve created a commission.',
+                    '**Commissions are paid**, so people will contact you to offer their services.',
+                    'If you believe you\'ve made a mistake in tagging it as **Paid**, delete this post and create a new one with the **Free** tag.',
+                    '**I\'ll provide you with some recommendations regarding commissions:**',
+                    '- Don\'t rush! You\'ll receive many requests, take your time to review the best offer. The first person who contacts you may not always be the best option.',
+                    `- We recommend accepting commissions from people with these roles, as they are qualified for commissions and you can avoid scams. ${MessageRoles}`,
+                    '- If you encounter any issues with a member related to a commission (scam, failure to fulfill terms, etc.), I recommend reporting them to the administrative team to assess whether sanctions should be applied.',
+                    '- **Does the bot delete your messages?** You probably don\'t have the necessary roles to participate in commissions.'
+                ]
+            };
+
             // Create message Embed
-            let MessageEmbed = new Discord.EmbedBuilder()
-                .setDescription(
-                    "Hello! <@" + Threader.ownerId +">\n" +
-                    "I've noticed that you've created a commission.\n" +
-                    "**Commissions are paid**, so people will contact you to offer their services.\n" +
-                    "If you believe you've made a mistake in tagging it as 'Paid', use the command **/deleteme.**\n\n" +
-                    "**I'll provide you with some recommendations regarding commissions:**\n" +
-                    "- Don't rush! You'll receive many requests, take your time to review the best offer. The first person who contacts you may not always be the best option.\n" +
-                    "- We recommend accepting commissions from people with these roles, as they are qualified for commissions and you can avoid scams. " + MessageRoles + "\n" +
-                    "- If you encounter any issues with a member related to a commission (scam, failure to fulfill terms, etc.), I recommend reporting them to the administrative team to assess whether sanctions should be applied.\n" +
-                    "- **Does the bot delete your messages?** You probably don't have the necessary roles to participate in commissions."
-                )
-                .setColor(`Yellow`);
+            let MessageEmbed = Client.botUtils.createEmbed(embedData);
             await Threader.send({ embeds: [MessageEmbed] });
 
         } catch(e) {
