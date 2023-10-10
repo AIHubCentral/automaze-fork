2de5418a34f6337f6bc0ef88e130cd32cd0ac5bb
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-10-10 16:25:47 -0300

    fix typo in username and use embed utility function in -overtrain

--- Commands/Tags/overtrain.js
+++ Commands/Tags/overtrain.js
@@ -1,5 +1,3 @@
-const { EmbedBuilder } = require("discord.js")
-
 module.exports = {
     name: 'overtrain',
     category: 'Tags',
@@ -17,19 +15,22 @@ module.exports = {
         const targetChannelId = client.discordIDs.Forum.Guides;
         const targetChannel = message.guild.channels.cache.get(targetChannelId) ?? '"❔┋guides" channel';
 
-        const embedDescription = [];
-        embedDescription.push('## All-In-One Guide on how to make a good model');
-        embedDescription.push('This guide explains how the **D** and **G** files works and much more: https://rentry.org/RVC_making-models\n\nCredits: **[LUSBERT](https://discordapp.com/users/917711764571951144)** <:lusbertmoment:1159804751924432906>');
-        embedDescription.push(`## Automated Overtraining Detection (AOD)\n> Will be available soon in ${targetChannel}\n\nCredits: **[grvscale](https://discordapp.com/users/590867867247837203)**`);
+        // old image (`https://media.discordapp.net/attachments/1124354352124010536/1126132264569417728/image.png`)
 
-        const embed = new EmbedBuilder()
-            .setTitle(`How do I know if my model is overtaining?`)
-            //.setImage(`https://media.discordapp.net/attachments/1124354352124010536/1126132264569417728/image.png`)
-            .setDescription(embedDescription.join('\n'))
-            .setColor(`Yellow`);
+        const embedData = {
+            title: 'How do I know if my model is overtaining?',
+            color: client.botConfigs.colorTheme.primary,
+            description: [
+                '## All-In-One Guide on how to make a good model',
+                'This guide explains how the **D** and **G** files works and much more: https://rentry.org/RVC_making-models\n\nCredits: **[LUSBERT](https://discordapp.com/users/917711764571951144)** <:lusbertmoment:1159804751924432906>',
+                `## Automated Overtraining Detection (AOD)\n> Will be available soon in ${targetChannel}\n\nCredits: **[grvyscale](https://discordapp.com/users/590867867247837203)**`
+            ]
+        };
+
+        const embed = client.botUtils.createEmbed(embedData);
 
         if (message.mentions.members.first()) {
-            return message.channel.send({content: `*Tag suggestion for ${message.mentions.members.first()}*`, embeds: [embed]});
+            return message.channel.send({content: `Hey, ${message.mentions.members.first()}!\n\n👇 Here are some resources to help you identify if your model is overtraining`, embeds: [embed]});
         }
 
         message.channel.send({embeds: [embed]});
