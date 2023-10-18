aaff2163e5c89075691f72575471171cc05f0130
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-10-18 19:33:26 -0300

    add brazilian guides to -rvc br

--- Commands/Tags/rvc.js
+++ Commands/Tags/rvc.js
@@ -12,19 +12,40 @@ module.exports = {
      * @param {String} prefix 
      */
     run: (client, message, args, prefix) => {
-
-        console.log(args);
-
-        const embeds = [
+        // remove mentions
+        const filteredArgs = args.filter(str => !str.includes('@'));
+        let messageTitle = 'RVC Guides (How to Make AI Cover)';
+        let messageContent = 'Here are some useful resources to help you learn how to make ai covers';
+        let messageBlooper = 'bruh i know how to make ai cover';
+        
+        // defaults to english
+        let embeds = [
             client.botUtils.createEmbed(client.botData.embeds.rvc.en.docs),
             client.botUtils.createEmbed(client.botData.embeds.rvc.en.guides),
             client.botUtils.createEmbed(client.botData.embeds.rvc.en.translations),
         ];
 
-        if (message.mentions.members.first()) {
-            return message.channel.send({content: `Hey, ${message.mentions.members.first()}!\n\n👇 Here are some useful resources to help you learn how to make ai covers`, embeds: embeds});
+        // get the first argument
+        if (filteredArgs.length > 0) {
+            switch(filteredArgs[0]) {
+                case 'br':
+                    embeds = [client.botUtils.createEmbed(client.botData.embeds.rvc.pt)];
+                    messageTitle = 'Guias RVC (como fazer cover com IA)';
+                    messageContent = 'Veja essas recomendações abaixo';
+                    messageBlooper = 'Eu sei fazer covers rsrs';
+                    break;
+            }
         }
 
-        message.channel.send({content: "# RVC Guides (How to Make AI Cover)", embeds: embeds});
+        if (message.mentions.members.first()) {
+            // check if mentioned the bot
+            if (client.user.id === message.mentions.members.first().id) {
+                return message.reply(messageBlooper);
+            }
+
+            return message.channel.send({content: `Hey, ${message.mentions.members.first()}!\n\n👇 ${messageContent}`, embeds: embeds});
+        }
+
+        message.channel.send({content: `# ${messageTitle}`, embeds: embeds});
     }
 }
