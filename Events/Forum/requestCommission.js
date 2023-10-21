cef2e316d740b0be270686bf9e8347f5d9c47e08
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-10-21 18:07:38 -0300

    show a message when someone creates a free model request

--- Events/Forum/requestCommission.js
+++ Events/Forum/requestCommission.js
@@ -1,60 +1,75 @@
-const Discord = require('discord.js');
 const { delay } = require(process.cwd() + '/utils.js');
 
 module.exports = {
     name: "threadCreate",
     once: false,
-    async run(Client, Threader, newlyCreated){
-        try{
-            // Quit if configuration doesn't allow bot to send messages
-            if(!Client.botConfigs.commissions.sendMessages) return;
+    async run(client, Threader, newlyCreated) {
+        try {
+            // quit if configuration doesn't allow bot to send messages
+            if (!client.botConfigs.commissions.sendMessages) return;
 
-            // Check is a Request Forum
-            if(!newlyCreated) return;
-            if(Threader.guildId != Client.discordIDs.Guild) return;
-            if(Threader.parentId != Client.discordIDs.Forum.RequestModel.ID) return;
-            if(!(Threader.appliedTags.find( Tag => Tag == Client.discordIDs.Forum.RequestModel.Tags.Paid))) return;
+            // check if it's a Request Forum
+            if (!newlyCreated) return;
+            if (Threader.guildId != client.discordIDs.Guild) return;
+            if (Threader.parentId != client.discordIDs.Forum.RequestModel.ID) return;
 
-            // Check if exists the channel
+            // check if the channel exists
             await delay(2000);
-            if(!(await Threader.guild.channels.cache.get(Threader.id))) return;
+            if (!(await Threader.guild.channels.cache.get(Threader.id))) return;
 
-            // Create tags message
-            let MessageRoles = "(";
-            for(let RolName of Client.discordIDs.Forum.RequestModel.ComissionAllow){
-                MessageRoles += "<@&" + Client.discordIDs.Roles[RolName] + ">, ";
-            }
-            MessageRoles = MessageRoles.slice(0, -2) + ")";
+            // check if it's a free or paid request
+            let isPaidRequest = Threader.appliedTags.find(Tag => Tag == client.discordIDs.Forum.RequestModel.Tags.Paid);
+            let isFreeRequest = Threader.appliedTags.find(Tag => Tag == client.discordIDs.Forum.RequestModel.Tags.Free);
 
-            // Create embeds
-            const embeds = [];
+            if (isPaidRequest) {
+                // Create tags message
+                let MessageRoles = "(";
+                for (let RolName of client.discordIDs.Forum.RequestModel.ComissionAllow) {
+                    MessageRoles += "<@&" + client.discordIDs.Roles[RolName] + ">, ";
+                }
+                MessageRoles = MessageRoles.slice(0, -2) + ")";
 
-            embeds.push(Client.botUtils.createEmbed({
-                color: 'Blurple',
-                description: [
-                    `Hello, <@${Threader.ownerId}>!`,
-                    '\nPeople will contact you to offer their services. However, if you created a **paid** request by mistake, just delete the post and create a new one with the tag **"free"**.',
-                    '\n**Here are some general recommendations regarding commissions:**',
-                    '- Don\'t rush! You\'ll receive many requests, take your time to review the best offer. The first person who contacts you may not always be the best option.',
-                    `- We recommend accepting commissions from people with these roles, as they are qualified for commissions and you can avoid scams. ${MessageRoles}`,
-                    '- If you encounter any issues with a member related to a commission (scam, failure to fulfill terms, etc.), please report it to the administrative team to assess whether sanctions should be applied.',
-                ]
-            }));
+                // Create embeds
+                const embeds = [];
 
-            if (Client.botConfigs.commissions.deleteMessages) {
-                embeds.push(Client.botUtils.createEmbed({
-                    title: '⚠️ Warning to model makers',
-                    description: ['> Make sure you have the **model master** role or your response might be deleted.']
+                embeds.push(client.botUtils.createEmbed({
+                    color: 'Blurple',
+                    description: [
+                        `Hello, <@${Threader.ownerId}>!`,
+                        '\nPeople will contact you to offer their services. However, if you created a **paid** request by mistake, just delete the post and create a new one with the tag **"free"**.',
+                        '\n**Here are some general recommendations regarding commissions:**',
+                        '- Don\'t rush! You\'ll receive many requests, take your time to review the best offer. The first person who contacts you may not always be the best option.',
+                        `- We recommend accepting commissions from people with these roles, as they are qualified for commissions and you can avoid scams. ${MessageRoles}`,
+                        '- If you encounter any issues with a member related to a commission (scam, failure to fulfill terms, etc.), please report it to the administrative team to assess whether sanctions should be applied.',
+                    ]
                 }));
+
+                if (client.botConfigs.commissions.deleteMessages) {
+                    embeds.push(client.botUtils.createEmbed({
+                        title: '⚠️ Warning to model makers',
+                        description: ['> Make sure you have the **model master** role or your response might be deleted.']
+                    }));
+                }
+
+                await Threader.send({ embeds: embeds });
             }
 
-            await Threader.send({ embeds: embeds });
-
-        } catch(e) {
-        
+            // the !isPiadRequest check is used here to deal with cases where they select both free and paid tags
+            if (isFreeRequest && !isPaidRequest) {
+                const embeds = [
+                    client.botUtils.createEmbed({
+                        color: client.botConfigs.colors.theme.tertiary,
+                        description: [
+                            '💡 **Tip**: You can try using the `/search` command from <@1150230843214794822> to check if someone already made this model.'
+                        ]
+                    })
+                ];
+                await Threader.send({ embeds: embeds });
+            }
+        } catch (e) {
             // Oh no, dat error.
             console.log(e);
-        
-        } 
+
+        }
     }
 }
