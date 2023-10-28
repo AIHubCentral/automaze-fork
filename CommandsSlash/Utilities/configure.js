bd05bb6529a52f8bdbbc5188b436358e3b5a91b1
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-10-28 11:29:46 -0300

    add command to configure color theme

--- CommandsSlash/Utilities/configure.js
+++ CommandsSlash/Utilities/configure.js
@@ -23,6 +23,26 @@ module.exports = {
                         .setDescription('Whether the bot should delete messages from users that doesnt have appropriate roles')
                         .setRequired(true)
                 )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('colors')
+                .setDescription('Configure color theme')
+                .addStringOption(option =>
+                    option
+                        .setName('primary')
+                        .setDescription('Primary color')
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('secondary')
+                        .setDescription('Secondary color')
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('tertiary')
+                        .setDescription('Tertiary color')
+                )
         ),
     async execute(interaction) {
         const client = interaction.client;
@@ -35,6 +55,35 @@ module.exports = {
             client.botConfigs.commissions.deleteMessages = deleteMessages;
 
             await interaction.reply({ content: `Bot configured:\n- sendMessages: **${sendMessages}**\n- deleteMessages: **${deleteMessages}**`, ephemeral: true});
+        }
+        else if (interaction.options.getSubcommand() === 'colors') {
+            const primaryColor = interaction.options.getString('primary');
+            const secondaryColor = interaction.options.getString('secondary');
+            const tertiaryColor = interaction.options.getString('tertiary');
+
+            let output = [];
+            let botResponse = 'No color was updated.';
+
+            if (primaryColor) {
+                client.botConfigs.colors.theme.primary = primaryColor;
+                output.push(`- Primary color: ${primaryColor}`);
+            }
+
+            if (secondaryColor) {
+                client.botConfigs.colors.theme.secondary = secondaryColor;
+                output.push(`- Secondary color: ${secondaryColor}`);
+            }
+
+            if (tertiaryColor) {
+                client.botConfigs.colors.theme.tertiary = tertiaryColor;
+                output.push(`- Tertiary color: ${tertiaryColor}`);
+            }
+
+            if (output.length > 0) {
+                botResponse = 'Colors updated:\n' + output.join('\n');
+            }
+
+            await interaction.reply({ content: botResponse, ephemeral: true })
         }
     }
 }
