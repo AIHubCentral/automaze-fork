5aca2bbec50096ce076fd1f9594a12ee78d15b2c
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-10-28 11:12:49 -0300

    show bananas in -inv

--- Commands/Game/items.js
+++ Commands/Game/items.js
@@ -17,16 +17,32 @@ module.exports = {
         let description = [];
         const member = message.mentions.members.first() || message.member;
 
-        if (!client.items.has(member.user.id) || !Object.values(client.items.get(member.user.id)).filter(entry => entry.value).length) {
+        // show bananas in inventory
+        const inventory = await client.knexInstance('inventory').where({
+            'user_id': member.user.id,
+            'item_id': 1
+        });
+
+        if (inventory.length === 1) {
+            let item = inventory[0];
+            let itemQuantity = item.quantity;
+            let itemDisplay = '- **Banana**: ' + item.quantity;
+            if (itemQuantity > 1) {
+                itemDisplay = itemDisplay.replace('Banana', 'Bananas');
+            }
+            description.push(itemDisplay);
+        }
+
+        if (inventory.length === 0 && (!client.items.has(member.user.id) || !Object.values(client.items.get(member.user.id)).filter(entry => entry.value).length)) {
             description.push(`Nothing to see here :(`);
         } else {
-            description.push(Object.values(client.items.get(member.user.id)).filter(entry => entry.value).map(entry => `**‣ ${entry.name}**: ${entry.value}`).join(`\n`));
+            //description.push(Object.values(client.items.get(member.user.id)).filter(entry => entry.value).map(entry => `**‣ ${entry.name}**: ${entry.value}`).join(`\n`));
         }
 
         const embed = new EmbedBuilder()
-        .setAuthor({name: member.user.username, iconURL: member.user.avatarURL()})
-        .setDescription(`## Items\n${description.join(`\n`)}`)
-        .setColor(`Green`);
+            .setAuthor({name: member.user.username, iconURL: member.user.avatarURL()})
+            .setDescription(`## Items\n${description.join(`\n`)}`)
+            .setColor(client.botConfigs.colors.theme.primary);
 
         message.reply({embeds: [embed]})
     }
