790d789230508c54758e9b15afcd29626fd8ba8c
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-11-14 10:47:44 -0300

    show banana counter in /topbanana

--- CommandsSlash/Fun/topBanana.js
+++ CommandsSlash/Fun/topBanana.js
@@ -1,45 +1,46 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    category: `Fun`,
-    cooldown: 15,
-    type: `slash`, 
-    data: new SlashCommandBuilder()
-                .setName('topbanana')
-                .setDescription('SEE HOW MUCH SOMEONE GOT BANAN!!!!11!111!11'),
-    async execute(interaction) {
-        const client = interaction.client;
+	category: 'Fun',
+	cooldown: 15,
+	type: 'slash',
+	data: new SlashCommandBuilder()
+		.setName('topbanana')
+		.setDescription('SEE HOW MUCH SOMEONE GOT BANAN!!!!11!111!11'),
+	async execute(interaction) {
+		const client = interaction.client;
 
-        if (client.disallowedChannelIds.includes(interaction.channelId)) {
-            await interaction.reply({ content: 'This command is not available here.', ephemeral: true});
-            return;
-        }
+		if (client.disallowedChannelIds.includes(interaction.channelId)) {
+			await interaction.reply({ content: 'This command is not available here.', ephemeral: true });
+			return;
+		}
 
-        await interaction.deferReply();
+		await interaction.deferReply();
 
-        const embedData = {
-            title: 'THE FORTNITE BALLS LEADERBANAN',
-            color: 'Yellow',
-            timestamp: true,
-            description: []
-        };
+		const embedData = {
+			title: 'THE FORTNITE BALLS LEADERBANAN',
+			color: 'Yellow',
+			timestamp: true,
+			description: [],
+		};
 
-        // fetch inventory from database
-        const inventory = await client.knexInstance('inventory').orderBy('quantity', 'desc').limit(15);
+		// fetch inventory from database
+		const inventory = await client.knexInstance('inventory').orderBy('quantity', 'desc').limit(15);
 
-        if (inventory.length === 0) {
-            embedData.description.push('> The leaderboard is empty, `/banana` someone to show results here!');
-            return await interaction.editReply({ embeds: [client.botUtils.createEmbed(embedData)] });
-        }
+		if (inventory.length === 0) {
+			embedData.description.push('> The leaderboard is empty, `/banana` someone to show results here!');
+			return await interaction.editReply({ embeds: [client.botUtils.createEmbed(embedData)] });
+		}
 
-        let rankCounter = 1;
-        for (const entry of inventory) {
-            let user = await client.knexInstance('user').where('id', entry['user_id']);
-            //embedData.description.push(`${rankCounter}. ${user[0].username} — ${entry.quantity}`);
-            embedData.description.push(`${rankCounter}. [${user[0].display_name ?? user[0].username}](https://discordapp.com/users/${user[0].id})`);
-            rankCounter++;
-        }
-        
-        await interaction.editReply({ embeds: [client.botUtils.createEmbed(embedData)] })
-    }
-}
+		let rankCounter = 1;
+		for (const entry of inventory) {
+			const user = await client.knexInstance('user').where('id', entry['user_id']);
+			const userDisplay = user[0].display_name ?? user[0].username;
+			const userProfileLink = 'https://discordapp.com/users/' + user[0].id;
+			embedData.description.push(`${rankCounter}. [${userDisplay}](${userProfileLink}) — ${entry.quantity}`);
+			rankCounter++;
+		}
+
+		await interaction.editReply({ embeds: [client.botUtils.createEmbed(embedData)] });
+	},
+};
