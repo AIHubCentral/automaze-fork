b85320baac31820a50c5f2a347f68e84e6fcaaed
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-11-13 21:03:23 -0300

    add eslint to the project

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 // Remember I will create the .env file if it is not already created
-const Env = require('dotenv').config();
+require('dotenv').config();
 
 // Libraries
-const Enmap = require("enmap");
-const Discord = require(`discord.js`);
+const Enmap = require('enmap');
+const Discord = require('discord.js');
 
 // Exports
 const { getAllFiles, createEmbed, createEmbeds, getAvailableColors, getRandomNumber, getRandomFromArray } = require('./utils');
 
 // JSONs
 const DiscordIDs = {
-    prod: "./Configs/idsDiscordProd.json",
-    dev: "./Configs/idsDiscordDev.json"
-}
-
-const itemsData = require('./JSON/items.json');
+	prod: './Configs/idsDiscordProd.json',
+	dev: './Configs/idsDiscordDev.json',
+};
 
 // Discord Client initialization
 const client = new Discord.Client({
-    intents: [
-        Discord.GatewayIntentBits.Guilds,
-        Discord.GatewayIntentBits.GuildMessages,
-        Discord.GatewayIntentBits.GuildMessageReactions,
-        Discord.GatewayIntentBits.MessageContent,
-        Discord.GatewayIntentBits.GuildModeration,
-        Discord.GatewayIntentBits.GuildMembers
-    ],
-    allowedMentions: {
-        parse: [`users`]
-    }
+	intents: [
+		Discord.GatewayIntentBits.Guilds,
+		Discord.GatewayIntentBits.GuildMessages,
+		Discord.GatewayIntentBits.GuildMessageReactions,
+		Discord.GatewayIntentBits.MessageContent,
+		Discord.GatewayIntentBits.GuildModeration,
+		Discord.GatewayIntentBits.GuildMembers,
+	],
+	allowedMentions: {
+		parse: ['users'],
+	},
 });
 
 // set dev=true in .env to use the development guild ids
@@ -38,9 +36,9 @@ const devMode = process.env.dev;
 client.discordIDs = require(devMode ? DiscordIDs.dev : DiscordIDs.prod);
 
 // Discord Tables
-client.forumSpammer = {}
+client.forumSpammer = {};
 
-// Discord Collections 
+// Discord Collections
 client.commands = new Discord.Collection();
 client.slashCommands = new Discord.Collection();
 client.contextMenuCommands = new Discord.Collection();
@@ -53,48 +51,48 @@ client.esnipes = new Discord.Collection();
 */
 
 client.cooldowns = {
-    banana: new Discord.Collection(),
-    slashCommands: new Discord.Collection(),
-    reactions: new Discord.Collection()
+	banana: new Discord.Collection(),
+	slashCommands: new Discord.Collection(),
+	reactions: new Discord.Collection(),
 };
 
 // Enmap's creation
-client.items = new Enmap({name: 'items'});
-client.currencies = new Enmap({name: 'currencies'});
-client.equipments = new Enmap({name: 'equipments'});
-client.models = new Enmap({name: 'models'});
-client.levels = new Enmap({name: 'levels'});
-client.exp = new Enmap({name: 'exp'});
-client.epochs = new Enmap({name: 'epochs'});
-client.banana = new Enmap({name: 'banana'});
+client.items = new Enmap({ name: 'items' });
+client.currencies = new Enmap({ name: 'currencies' });
+client.equipments = new Enmap({ name: 'equipments' });
+client.models = new Enmap({ name: 'models' });
+client.levels = new Enmap({ name: 'levels' });
+client.exp = new Enmap({ name: 'exp' });
+client.epochs = new Enmap({ name: 'epochs' });
+client.banana = new Enmap({ name: 'banana' });
 client.scourCD = new Enmap();
-client.prefix = new Enmap({name: 'prefix'});
+client.prefix = new Enmap({ name: 'prefix' });
 
 client.botAdminIds = process.env.developerIds.split(',');
 
 client.botConfigs = require('./JSON/botConfigs.json');
 if (!devMode) {
-    // when in production environment, send a message on startup to the dev server
-    client.botConfigs.messageOnStartup = true;
+	// when in production environment, send a message on startup to the dev server
+	client.botConfigs.messageOnStartup = true;
 }
 
 // Utility functions and JSON data
 client.botData = {
-    embeds: require('./JSON/embeds.json'),
-    reactionKeywords: require('./JSON/reactionKeywords.json'),
-    cooldownImmuneUsers: new Discord.Collection(),
+	embeds: require('./JSON/embeds.json'),
+	reactionKeywords: require('./JSON/reactionKeywords.json'),
+	cooldownImmuneUsers: new Discord.Collection(),
 };
 
 client.botUtils = {
-    createEmbed,
-    createEmbeds,
-    getAvailableColors,
-    getRandomNumber,
-    getRandomFromArray
+	createEmbed,
+	createEmbeds,
+	getAvailableColors,
+	getRandomNumber,
+	getRandomFromArray,
 };
 
 // knex database
-client.knexInstance = require('./database/dbManager.js').createInstance('./database/knex.sqlite');;
+client.knexInstance = require('./database/dbManager.js').createInstance('./database/knex.sqlite');
 
 // bot responses loaded on startup
 client.botResponses = require('./JSON/bot_responses.json');
@@ -106,9 +104,9 @@ client.disallowedChannelIds.push(client.discordIDs.Channel.MakingModels);
 
 // Read all handlers of the folder
 const handlerFiles = getAllFiles('./Handlers').filter(file => file.endsWith('.js'));
-for(const handler of handlerFiles) {
-    require(handler)(client, Discord);
-};
+for (const handler of handlerFiles) {
+	require(handler)(client, Discord);
+}
 
 // Add your bot token in the token variable in the .env file (create it if it doesn't exist)
 // Then use dotenv to read the token from that file
