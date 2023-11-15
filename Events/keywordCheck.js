f916d0cfa4f12a19500f799b8c71805dbac236b9
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-11-15 11:55:43 -0300

    add frequency option to reactions and also text and sticker

--- Events/keywordCheck.js
+++ Events/keywordCheck.js
@@ -1,103 +1,130 @@
+/* eslint-disable indent */
 const { ChannelType } = require('discord.js');
+const { getChannelById } = require('../utils.js');
 const delay = require('node:timers/promises').setTimeout;
 
 module.exports = {
-    name: "messageCreate",
-    once: false,
-    async run(client, message, _) {
-        // only proceed if reactions is enabled in configs
-        if (!client.botConfigs.general.reactions) return;
+	name: 'messageCreate',
+	once: false,
+	async run(client, message) {
+		// only proceed if reactions is enabled in configs
+		if (!client.botConfigs.general.reactions) return;
 
-        // don't react to itself
-        if (message.author.bot) return;
+		// don't react to itself
+		if (message.author.bot) return;
 
-        // don't react to threads
-        if (message.channel.type === ChannelType.PublicThread) return;
+		// don't react to threads
+		if (message.channel.type === ChannelType.PublicThread) return;
 
-        // skip prefix commands
-        const prefix = client.prefix.ensure(message.guild.id, '-');
-        if (message.content.startsWith(prefix)) return;
+		// skip prefix commands
+		const prefix = client.prefix.ensure(message.guild.id, '-');
+		if (message.content.startsWith(prefix)) return;
 
-        // check if user is on cooldown
-        if (client.cooldowns.reactions.has(message.author.id)) {
-            const cooldownExpiration = client.cooldowns.reactions.get(message.author.id);
-            const currentDate = new Date();
+		// check if user is on cooldown
+		if (client.cooldowns.reactions.has(message.author.id)) {
+			const cooldownExpiration = client.cooldowns.reactions.get(message.author.id);
+			const currentDate = new Date();
 
-            if (currentDate.getTime() < cooldownExpiration.getTime()) {
-                // user is on cooldown, don't add reactions
-                return;
-            }
-            else {
-                // cooldown expired
-                client.cooldowns.reactions.delete(message.author.id);
-            }
-        }
+			if (currentDate.getTime() < cooldownExpiration.getTime()) {
+				// user is on cooldown, don't add reactions
+				return;
+			}
+			else {
+				// cooldown expired
+				client.cooldowns.reactions.delete(message.author.id);
+			}
+		}
 
-        // don't add reaction if there are attachments
-        if (message.attachments.size > 0) return;
+		// don't add reaction if there are attachments
+		if (message.attachments.size > 0) return;
 
-        const messageLowercase = message.content.toLowerCase();
+		const messageLowercase = message.content.toLowerCase();
 
-        // don't react if keyword is in an url
-        if (messageLowercase.includes('http')) return;
+		// don't react if keyword is in an url
+		if (messageLowercase.includes('http')) return;
 
-        try {
-            // max of 20 reactions
-            let reactionCounter = 0;
-            for (const item of client.botData.reactionKeywords) {
-                if (reactionCounter >= 20) break;
+		try {
+			let regex;
+			let foundKeyword = false;
+			let matchedKeywords = 0;
 
-                /*
-                let foundKeyword = false;
+			// max of 20 reactions
+			let reactionCounter = 0;
+			for (const item of client.botData.reactionKeywords) {
+				if (reactionCounter >= 20) break;
 
-                let keywordCount = item.keywords.length;
-                let matchedKeywords = 0;
-                */
+				let randomNumber = 0;
+				let shouldProceed = true;
 
-                let regex;
+				if (item.frequency) {
+					if (item.frequency === 'rare') {
+						randomNumber = client.botUtils.getRandomNumber(0, 100);
+					}
+					else {
+						randomNumber = client.botUtils.getRandomNumber(0, 10);
+					}
 
-                for (const keyword of item.keywords) {
-                    // text is exact the keyword
-                    if (item.exact) {
-                        foundKeyword = messageLowercase === keyword;
-                    }
-                    else {
-                        // otherwise check if text includes the keyword
-                        //foundKeyword = messageLowercase.includes(keyword);
-                        regex = new RegExp(`(?<!:)\\b${keyword}\\b`);
-                        foundKeyword = regex.test(messageLowercase);
-                    }
+					shouldProceed = randomNumber === 0;
+				}
 
-                    if (foundKeyword) {
-                        /*
-                        matchedKeywords++;
+				if (!shouldProceed) continue;
 
-                        // only needs to match one keyword
-                        if (matchedKeywords > 1) break;
+				for (const keyword of item.keywords) {
+					// text is exact the keyword
+					if (item.exact) {
+						foundKeyword = messageLowercase === keyword;
+					}
+					else {
+						// otherwise check if text includes the keyword
+						// foundKeyword = messageLowercase.includes(keyword);
+						regex = new RegExp(`(?<!:)\\b${keyword}\\b`);
+						foundKeyword = regex.test(messageLowercase);
+					}
 
-                        //console.log('found', item);
-                        //console.log('matched keywords', matchedKeywords);
-                        */
+					if (foundKeyword) {
+						matchedKeywords++;
 
-                        for (const emoji of item.emojis) {
-                            await message.react(emoji);
-                            await delay(1500);
-                            reactionCounter++;
-                        }
-                    }
-                }
-                //console.log('End of keyword check');
-            }
-        } catch (error) {
-            console.log('Failed to add reaction');
-            console.error(error);
-            // sends a log to the dev server
-            if (client.botConfigs.general.sendLogs) {
-                const { botConfigs } = client;
-                const devServerGuild = client.guilds.cache.get(botConfigs.devServerId);
-                const botDebugChannel = devServerGuild.channels.cache.get(botConfigs.debugChannelId);
-                await botDebugChannel.send(`Failed to add reaction:\n> [Go to message](https://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id})`);
-            }
-        }
-    }
-}
+						// only needs to match one keyword
+						if (matchedKeywords > 1) break;
+
+						// console.log('found', item);
+						const botResponse = { allowedMentions: { repliedUser: true } };
+
+						switch (item.kind) {
+							case 'sticker':
+								botResponse.stickers = [item.stickerId];
+								await delay(3_000);
+								await message.reply(botResponse);
+								break;
+							case 'text':
+								botResponse.content = client.botUtils.getRandomFromArray(item.responses);
+								await message.channel.sendTyping();
+								await delay(botResponse.content.length * 350);
+								await message.reply(botResponse);
+								break;
+							default:
+								for (const emoji of item.emojis) {
+									await message.react(emoji);
+									await delay(2000);
+									reactionCounter++;
+								}
+						}
+					}
+				}
+				// console.log('End of keyword check');
+			}
+		}
+		catch (error) {
+			console.log('Failed to add reaction');
+			console.error(error);
+			// sends a log to the dev server
+			if (client.botConfigs.general.sendLogs) {
+				const { botConfigs } = client;
+				const devServerGuild = client.guilds.cache.get(botConfigs.devServerId);
+				const botDebugChannel = await getChannelById(botConfigs.debugChannelId, devServerGuild);
+				const messageLink = `https://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`;
+				await botDebugChannel.send(`Failed to add reaction:\n> [Go to message](${messageLink})`);
+			}
+		}
+	},
+};
