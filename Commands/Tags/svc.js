aafd56817ce8bf8f8061a6a0aba4a7d5062e4989
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-11-16 12:59:17 -0300

    add easter eggs to -svc

--- Commands/Tags/svc.js
+++ Commands/Tags/svc.js
@@ -1,19 +1,37 @@
+const delay = require('node:timers/promises').setTimeout;
+
 module.exports = {
-    name: 'svc',
-    category: 'Tags',
-    description: 'why do you still use svc',
-    aliases: [],
-    syntax: `svc`,
-    /**
-     * 
-     * @param {Client} client 
-     * @param {Message} message 
-     * @param {string[]} args 
-     * @param {String} prefix 
-     */
-    run: (client, message, args, prefix) => {
-        const { botResponses, botUtils } = client;
-        const selectedMessage = botUtils.getRandomFromArray(botResponses.responses.svc);
-        message.reply({ content: selectedMessage });
-    }
-}
+	name: 'svc',
+	category: 'Tags',
+	description: 'why do you still use svc',
+	aliases: [],
+	syntax: 'svc',
+	/**
+	 *
+	 * @param {Client} client
+	 * @param {Message} message
+	 * @param {string[]} args
+	 * @param {String} prefix
+	 */
+	run: async (client, message) => {
+		const { botResponses, botUtils } = client;
+		const botResponse = {};
+		const selectedMessage = botUtils.getRandomFromArray(botResponses.responses.svc);
+		botResponse.content = selectedMessage;
+		await message.channel.sendTyping();
+		await delay(selectedMessage.length * 350);
+		if (message.mentions.members.first()) {
+			botResponse.allowedMentions = { repliedUser: true };
+			// if mentioned a bot send 'how to make ai cover sticker'
+			console.log(message.mentions.members.first());
+			if (message.mentions.members.first().user.bot) {
+				botResponse.content = '';
+				botResponse.stickers = ['1159360069557813268'];
+				return message.reply(botResponse);
+			}
+			botResponse.content = `${message.mentions.members.first()} ${selectedMessage}`;
+			return message.channel.send(botResponse);
+		}
+		message.channel.send(botResponse);
+	},
+};
