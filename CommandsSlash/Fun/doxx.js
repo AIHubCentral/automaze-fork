b3ffaaf83fe114be5a7294fe3a19a78371fb4d60
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-11-16 13:14:35 -0300

    don't allow doxxing bots

--- CommandsSlash/Fun/doxx.js
+++ CommandsSlash/Fun/doxx.js
@@ -1,87 +1,88 @@
-const Chance = require("chance");
+/* eslint-disable indent */
+const Chance = require('chance');
 const chance = new Chance;
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    category: `Fun`,
-    cooldown: 60,
-    type: `slash`,   
-    data: new SlashCommandBuilder()
-                .setName('doxx')
-                .setDescription('NOT ACTUAL DOXXING. creates random ip and house address')
-                .addUserOption(option => 
-                    option
-                        .setName('user')
-                        .setDescription('User to doxx')
-                        .setRequired(true)
-                ),
-    async execute(interaction) {
+	category: 'Fun',
+	cooldown: 60,
+	type: 'slash',
+	data: new SlashCommandBuilder()
+		.setName('doxx')
+		.setDescription('NOT ACTUAL DOXXING. creates random ip and house address')
+		.addUserOption(option =>
+			option
+				.setName('user')
+				.setDescription('User to doxx')
+				.setRequired(true),
+		),
+	async execute(interaction) {
 
-        if (interaction.client.disallowedChannelIds.includes(interaction.channelId)) {
-            await interaction.reply({ content: 'This command is not available here.', ephemeral: true});
-            return;
-        }
+		if (interaction.client.disallowedChannelIds.includes(interaction.channelId)) {
+			await interaction.reply({ content: 'This command is not available here.', ephemeral: true });
+			return;
+		}
 
-        const targetUser = interaction.options.getUser('user');
+		const targetUser = interaction.options.getUser('user');
 
-        let guildMember = interaction.guild.members.cache.get(targetUser.id);
+		let guildMember = interaction.guild.members.cache.get(targetUser.id);
 
-        if (!guildMember) {
-            console.log('Guild member not found in cache...Fetching');
-            guildMember = interaction.guild.members.fetch(targetUser.id);
-        }
+		if (!guildMember) {
+			console.log('Guild member not found in cache...Fetching');
+			guildMember = interaction.guild.members.fetch(targetUser.id);
+		}
 
-        const bot = interaction.client.user;
-        const oldAutomazeId = interaction.client.botResponses.userIds.oldAutomazeId;
+		const bot = interaction.client.user;
 
-        const [ip, ipv6, mac, address] = interaction.client.doxx.ensure(
-            targetUser.id, () => [chance.ip(), chance.ipv6(), chance.mac_address(), chance.address()]
-        );
+		const [ip, ipv6, mac, address] = interaction.client.doxx.ensure(
+			targetUser.id, () => [chance.ip(), chance.ipv6(), chance.mac_address(), chance.address()],
+		);
 
-        const fetchingEmbed = new EmbedBuilder()
-                                    .setTitle(`⏳ Fetching...`)
-                                    .setColor(`Yellow`);
+		const fetchingEmbed = new EmbedBuilder()
+			.setTitle('⏳ Fetching...')
+			.setColor('Yellow');
 
-        const reply = await interaction.reply({embeds: [fetchingEmbed]});
+		const reply = await interaction.reply({ embeds: [fetchingEmbed] });
 
-        let doxxData = {
-            'title': '',
-            'IP': 'N/A',
-            'IPv6': 'N/A',
-            'MAC': 'N/A',
-            'address': 'Not found',
-            'embedColor': 'Red'
-        };
+		const doxxData = {
+			'title': '❌ Failed to retrieve information!',
+			'IP': 'N/A',
+			'IPv6': 'N/A',
+			'MAC': 'N/A',
+			'address': 'Not found',
+			'embedColor': 'Red',
+			'duration': 6000,
+		};
 
-        switch(targetUser.id) {
-            case(bot.id):
-                doxxData.title =  '❌ yo i aint sharing my info';
-                doxxData.address = 'under the bridge';
-                break;
-            case(oldAutomazeId):
-                doxxData.title = '❌ Failed to retrieve information!';
-                break;
-            default:
-                doxxData.title = `✅ We found you **${guildMember.nickname ?? targetUser.username}**!`;
-                doxxData.IP = ip;
-                doxxData.IPv6 = ipv6;
-                doxxData.MAC = mac;
-                doxxData.address = address;
-                doxxData.embedColor = 'Green';
-        }
+		if (!targetUser.bot) {
+			doxxData.title = `✅ We found you **${guildMember.nickname ?? targetUser.displayName ?? targetUser.username}**!`;
+			doxxData.IP = ip;
+			doxxData.IPv6 = ipv6;
+			doxxData.MAC = mac;
+			doxxData.address = address;
+			doxxData.embedColor = 'Green';
+			doxxData.duration = 3000;
+		}
+		else if (targetUser.id === bot.id) {
+			// tried to doxx automaze...
+			doxxData.title = '❌ yo i aint sharing my info';
+			doxxData.address = 'under the bridge';
+		}
 
-        let embedDescription = `**IP**: ${doxxData.IP}`;
-        embedDescription += `\n**IPv6**: ${doxxData.IPv6}`;
-        embedDescription += `\n**MAC Address**: ${doxxData.MAC}`;
-        embedDescription += `\n**Address (not exact)**: ${doxxData.address}`;
-        embedDescription += `\n\nUsed: \`/doxx\` ${targetUser}`;
+		const embedDescription = [
+			`**IP**: ${doxxData.IP}`,
+			`**IPv6**: ${doxxData.IPv6}`,
+			`**MAC Address**: ${doxxData.MAC}`,
+			`**Address (not exact)**: ${doxxData.address}`,
+			`\nUsed: \`/doxx\` ${targetUser}`,
+		].join('\n');
 
-        const foundEmbed = new EmbedBuilder()
-                                .setTitle(doxxData.title)
-                                .setDescription(embedDescription)
-                                .setColor(doxxData.embedColor);
-        setTimeout(async () => {
-            await reply.edit({embeds: [foundEmbed]});
-        }, 3000)
-    }
-}
+		const foundEmbed = new EmbedBuilder()
+			.setTitle(doxxData.title)
+			.setDescription(embedDescription)
+			.setColor(doxxData.embedColor);
+		setTimeout(async () => {
+			await reply.edit({ embeds: [foundEmbed] });
+		}, doxxData.duration);
+	},
+};
