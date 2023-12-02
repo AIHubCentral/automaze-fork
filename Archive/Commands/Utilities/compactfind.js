1df8c66b52a1dc06646febff219c2fa1f869194c
Raytracer <141577659+RayTracerGC@users.noreply.github.com>
2023-12-02 19:48:03 -0300

    archive unused commands

--- Archive/Commands/Utilities/compactfind.js
+++ Archive/Commands/Utilities/compactfind.js
@@ -1,5 +1,3 @@
-const { EmbedBuilder } = require(`discord.js`);
-
 module.exports = {
     name: 'compactfind',
     category: 'Utilities',
@@ -14,52 +12,6 @@ module.exports = {
      * @param {String} prefix 
      */
     run: async (client, message, args, prefix) => {
-        if (client.disallowedChannelIds.includes(`${message.channelId}`)) {
-            if (message.content.startsWith('-cfind')) {
-                const botReply = await message.reply('This command is not available in this channel.');
-
-                setTimeout(async () => {
-                    await message.delete();
-                    await botReply.delete();
-                }, 6000);
-
-                return;
-            }
-        }
-
-        const query = message.content.split(' ').slice(1).join(' ');
-
-        if (!query) {
-            return void message.reply(`no query provided`);
-        }
-
-        let allResults = client.modelSearchEngine.search(query, { fuzzy: 0.2 }).filter(result => result.downloadURL && result.downloadURL.length);
-
-        allResults.sort((a, b) => b.score - a.score); // GPT-generated code, sort the results in descending order
-
-        let results;
-        let resultsLeft;
-
-        if (!allResults.length) {
-            return void message.channel.send({ embeds: [new EmbedBuilder().setTitle('No result found.').setColor(`Red`)] });
-        }
-
-        if (allResults.length > 3) {
-            results = allResults.slice(0, 3);
-            resultsLeft = allResults.length - 3;
-        } else {
-            results = allResults;
-        }
-
-        const resultEmbed = new EmbedBuilder()
-            .setTitle(`${allResults.length} results found - Search mode: R-D, fuzzy: 0.2, compact`)
-            .setDescription(results.map(result => `- [${result.title}](${result.downloadURL[0]}) ${result.tags.map(tag => `${tag ? tag.icon : `Deleted Icon`}`).join(``)} - ${result.creator}`).join(`\n`))
-            .setColor(`Green`);
-
-        if (resultsLeft) {
-            resultEmbed.setFooter({ text: `And ${resultsLeft} more results...` })
-        }
-
-        message.channel.send({ embeds: [resultEmbed] });
+        await message.reply(client.botResponses.responses.find);
     }
 }
