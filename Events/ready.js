24c4db34b1474abfc077a4746696feeaf9a4ba9a
RayTracerGC <141577659+RayTracerGC@users.noreply.github.com>
2024-03-12 18:02:21 -0300

    print nodejs version on startup

--- Events/ready.js
+++ Events/ready.js
@@ -1,32 +1,33 @@
 module.exports = {
-    name: "ready",
-    once: true,
-    async run(client){
-        console.log(`\nReady! Logged in as ${client.user.tag}`);
+	name: 'ready',
+	once: true,
+	async run(client) {
+		console.log(`\nReady! Logged in as ${client.user.tag}`);
+		console.log(`Node version: ${process.version}`);
 
-        const guilds = client.guilds.cache;
-        console.log(`\nBot is currently in ${guilds.size} guild(s):`);
+		const guilds = client.guilds.cache;
+		console.log(`\nBot is currently in ${guilds.size} guild(s):`);
 
-        guilds.forEach((guild) => {
-            console.log(`${guild.id}: ${guild.name}`);
-        });
+		guilds.forEach((guild) => {
+			console.log(`${guild.id}: ${guild.name}`);
+		});
 
-        const { botConfigs } = client;
+		const { botConfigs } = client;
 
-        if (botConfigs.messageOnStartup) {
-            const devServerGuild = client.guilds.cache.get(botConfigs.devServerId);
-            const botDebugChannel = devServerGuild.channels.cache.get(botConfigs.debugChannelId);
-            
-            if (!botDebugChannel) {
-                botDebugChannel = await devServer.channels.fetch(botConfigs.debugChannelId);
-                
-                if (!botDebugChannel) {
-                    console.log('Failed to fetch ', botConfigs.debugChannelId);
-                    return;
-                }
-            }
-            
-            await botDebugChannel.send('🟢 Bot is online!');
-        }
-    }
+		if (botConfigs.messageOnStartup) {
+			const devServerGuild = client.guilds.cache.get(botConfigs.devServerId);
+			let botDebugChannel = devServerGuild.channels.cache.get(botConfigs.debugChannelId);
+
+			if (!botDebugChannel) {
+				botDebugChannel = await devServerGuild.channels.fetch(botConfigs.debugChannelId);
+
+				if (!botDebugChannel) {
+					console.error('Failed to fetch ', botConfigs.debugChannelId);
+					return;
+				}
+			}
+
+			await botDebugChannel.send('🟢 Bot is online!');
+		}
+	},
 };
