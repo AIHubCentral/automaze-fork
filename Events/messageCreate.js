ee23b52c18c0ec3dc6e55996fac159f3ad3a9a1c
RayTracerGC <141577659+RayTracerGC@users.noreply.github.com>
2024-03-17 19:14:28 -0300

    log prefix command usage and when the bot is mentioned

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -1,59 +1,62 @@
 module.exports = {
-	name: 'messageCreate',
-	once: false,
-	async run(client, message) {
-		if (message.author.bot) return;
+    name: 'messageCreate',
+    once: false,
+    async run(client, message) {
+        if (message.author.bot) return;
 
-		// handle prefix commands first
-		const prefix = client.prefix;
+        // handle prefix commands first
+        const prefix = client.prefix;
 
-		if (message.content.startsWith(prefix)) {
-			const args = message.content.slice(prefix.length).trim().split(/ +/);
+        if (message.content.startsWith(prefix)) {
+            const args = message.content.slice(prefix.length).trim().split(/ +/);
 
-			// Use the command alias if there's any, if there's none use the real command name instead
-			const commandName = args.shift().toLowerCase();
-			const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+            // Use the command alias if there's any, if there's none use the real command name instead
+            const commandName = args.shift().toLowerCase();
+            const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-			if (command) {
-				command.run(client, message, args, prefix);
-			}
-		}
-		else {
-			const messageLowercase = message.content.toLowerCase();
+            if (command) {
+                client.logger.info(`Executing command (${commandName}) - guild:${message.guild.id};channel:${message.channel.id};type:prefix`);
+                command.run(client, message, args, prefix);
+            }
+        }
+        else {
+            const messageLowercase = message.content.toLowerCase();
 
-			// triggered only if bot is mentioned
-			if (message.content.includes('<@' + client.user.id + '>')) {
-				const devServerGuildId = '1136971905354711193';
+            // triggered only if bot is mentioned
+            if (message.content.includes('<@' + client.user.id + '>')) {
+                const devServerGuildId = '1136971905354711193';
 
-				const embedData = {
-					title: 'Wassup I\'m Automaze!',
-					color: client.botConfigs.colors.theme.primary,
-					description: [],
-				};
+                const embedData = {
+                    title: 'Wassup I\'m Automaze!',
+                    color: client.botConfigs.colors.theme.primary,
+                    description: [],
+                };
 
-				embedData.description.push(`\n- My prefix in this server is \`${prefix}\` (customizable with \`${prefix}prefix\`)`);
+                embedData.description.push(`\n- My prefix in this server is \`${prefix}\` (customizable with \`${prefix}prefix\`)`);
 
-				// only show how many guilds the bot is present if in the development server
-				if (message.guild.id == devServerGuildId) {
-					embedData.description.push(`- Currently I'm present in ${client.guilds.cache.size} servers.`);
-				}
+                // only show how many guilds the bot is present if in the development server
+                if (message.guild.id == devServerGuildId) {
+                    embedData.description.push(`- Currently I'm present in ${client.guilds.cache.size} servers.`);
+                }
 
-				embedData.description.push('- Interested in how I\'m built? [I\'m actually open source!](https://github.com/DeprecatedTable/automaze)');
-				// embedData.description.push('- Feeling a tad bit generous? [Buy me a coffee!](https://ko-fi.com/fungusdesu)');
-				embedData.description.push('- Forgot a specific command? Try `/help` or `-help`');
+                embedData.description.push('- Interested in how I\'m built? [I\'m actually open source!](https://github.com/DeprecatedTable/automaze)');
+                // embedData.description.push('- Feeling a tad bit generous? [Buy me a coffee!](https://ko-fi.com/fungusdesu)');
+                embedData.description.push('- Forgot a specific command? Try `/help` or `-help`');
 
-				const embed = client.botUtils.createEmbed(embedData);
-				await message.reply({ embeds: [embed] });
+                client.logger.info(`Bot mentioned - guild:${message.guild.id};channel:${message.channel.id}`);
 
-				return;
-			}
+                const embed = client.botUtils.createEmbed(embedData);
+                await message.reply({ embeds: [embed] });
 
-			// triggered on comission channel
-			if (message.channel.parentId == client.discordIDs.Forum.RequestModel.ID) {
-				if (message.channel.ownerId === message.author.id && messageLowercase.includes('taken')) {
-					await message.reply('**Tip**: You can use the `-close` command to lock this post.');
-				}
-			}
-		}
-	},
+                return;
+            }
+
+            // triggered on comission channel
+            if (message.channel.parentId == client.discordIDs.Forum.RequestModel.ID) {
+                if (message.channel.ownerId === message.author.id && messageLowercase.includes('taken')) {
+                    await message.reply('**Tip**: You can use the `-close` command to lock this post.');
+                }
+            }
+        }
+    },
 };
