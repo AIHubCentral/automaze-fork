2bc8fae157ff926c1024a5f6c3f74988516c2684
RayTracerGC <141577659+RayTracerGC@users.noreply.github.com>
2024-03-17 18:25:16 -0300

    use 4 spaces indentation instead of tabs on eslint

--- Events/ready.js
+++ Events/ready.js
@@ -1,33 +1,33 @@
 module.exports = {
-	name: 'ready',
-	once: true,
-	async run(client) {
-		client.logger.info(`Ready! Logged in as ${client.user.tag}`);
-		client.logger.info(`Node version: ${process.version}`);
+    name: 'ready',
+    once: true,
+    async run(client) {
+        client.logger.info(`Ready! Logged in as ${client.user.tag}`);
+        client.logger.info(`Node version: ${process.version}`);
 
-		const guilds = client.guilds.cache;
-		client.logger.info(`Bot is currently in ${guilds.size} guild(s):`);
+        const guilds = client.guilds.cache;
+        client.logger.info(`Bot is currently in ${guilds.size} guild(s):`);
 
-		guilds.forEach((guild) => {
-			client.logger.info(`${guild.id}: ${guild.name}`);
-		});
+        guilds.forEach((guild) => {
+            client.logger.info(`${guild.id}: ${guild.name}`);
+        });
 
-		const { botConfigs } = client;
+        const { botConfigs } = client;
 
-		if (botConfigs.messageOnStartup) {
-			const devServerGuild = client.guilds.cache.get(botConfigs.devServerId);
-			let botDebugChannel = devServerGuild.channels.cache.get(botConfigs.debugChannelId);
+        if (botConfigs.messageOnStartup) {
+            const devServerGuild = client.guilds.cache.get(botConfigs.devServerId);
+            let botDebugChannel = devServerGuild.channels.cache.get(botConfigs.debugChannelId);
 
-			if (!botDebugChannel) {
-				botDebugChannel = await devServerGuild.channels.fetch(botConfigs.debugChannelId);
+            if (!botDebugChannel) {
+                botDebugChannel = await devServerGuild.channels.fetch(botConfigs.debugChannelId);
 
-				if (!botDebugChannel) {
-					client.logger.error(`Failed to fetch ${botConfigs.debugChannelId}`);
-					return;
-				}
-			}
+                if (!botDebugChannel) {
+                    client.logger.error(`Failed to fetch ${botConfigs.debugChannelId}`);
+                    return;
+                }
+            }
 
-			await botDebugChannel.send('🟢 Bot is online!');
-		}
-	},
+            await botDebugChannel.send('🟢 Bot is online!');
+        }
+    },
 };
--- index.js
+++ index.js
@@ -8,31 +8,31 @@ const logger = require('./logger');
 
 // Exports
 const {
-	getAllFiles, createEmbed, createEmbeds,
-	getAvailableColors, getRandomNumber, getRandomFromArray,
-	Scheduler,
-	getThemes,
+    getAllFiles, createEmbed, createEmbeds,
+    getAvailableColors, getRandomNumber, getRandomFromArray,
+    Scheduler,
+    getThemes,
 } = require('./utils');
 
 // JSONs
 const DiscordIDs = {
-	prod: './JSON/configs/idsDiscordProd.json',
-	dev: './JSON/configs/idsDiscordDev.json',
+    prod: './JSON/configs/idsDiscordProd.json',
+    dev: './JSON/configs/idsDiscordDev.json',
 };
 
 // Discord Client initialization
 const client = new Discord.Client({
-	intents: [
-		Discord.GatewayIntentBits.Guilds,
-		Discord.GatewayIntentBits.GuildMessages,
-		Discord.GatewayIntentBits.GuildMessageReactions,
-		Discord.GatewayIntentBits.MessageContent,
-		Discord.GatewayIntentBits.GuildModeration,
-		Discord.GatewayIntentBits.GuildMembers,
-	],
-	allowedMentions: {
-		parse: ['users'],
-	},
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.GuildMessageReactions,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.GuildModeration,
+        Discord.GatewayIntentBits.GuildMembers,
+    ],
+    allowedMentions: {
+        parse: ['users'],
+    },
 });
 
 // winston logger
@@ -60,9 +60,9 @@ client.esnipes = new Discord.Collection();
 */
 
 client.cooldowns = {
-	banana: new Discord.Collection(),
-	slashCommands: new Discord.Collection(),
-	reactions: new Discord.Collection(),
+    banana: new Discord.Collection(),
+    slashCommands: new Discord.Collection(),
+    reactions: new Discord.Collection(),
 };
 
 const botConfigs = require('./JSON/botConfigs.json');
@@ -89,23 +89,23 @@ client.botAdminIds = process.env.developerIds.split(',');
 
 client.botConfigs = botConfigs;
 if (!devMode) {
-	// when in production environment, send a message on startup to the dev server
-	client.botConfigs.messageOnStartup = true;
+    // when in production environment, send a message on startup to the dev server
+    client.botConfigs.messageOnStartup = true;
 }
 
 // Utility functions and JSON data
 client.botData = {
-	embeds: require('./JSON/embeds.json'),
-	reactionKeywords: require('./JSON/reactionKeywords.json'),
-	cooldownImmuneUsers: new Discord.Collection(),
+    embeds: require('./JSON/embeds.json'),
+    reactionKeywords: require('./JSON/reactionKeywords.json'),
+    cooldownImmuneUsers: new Discord.Collection(),
 };
 
 client.botUtils = {
-	createEmbed,
-	createEmbeds,
-	getAvailableColors,
-	getRandomNumber,
-	getRandomFromArray,
+    createEmbed,
+    createEmbeds,
+    getAvailableColors,
+    getRandomNumber,
+    getRandomFromArray,
 };
 
 // load themes on startup
@@ -130,7 +130,7 @@ client.disallowedChannelIds.push(client.discordIDs.Channel.MakingModels);
 // Read all handlers of the folder
 const handlerFiles = getAllFiles('./Handlers').filter(file => file.endsWith('.js'));
 for (const handler of handlerFiles) {
-	require(handler)(client, Discord);
+    require(handler)(client, Discord);
 }
 
 // Add your bot token in the token variable in the .env file (create it if it doesn't exist)
