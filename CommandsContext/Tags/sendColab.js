d81162cb6551dd55f404e49fb784373a96ced66c
RayTracerGC <141577659+RayTracerGC@users.noreply.github.com>
2024-03-17 19:05:05 -0300

    fix 'send colab links' context command not working

--- CommandsContext/Tags/sendColab.js
+++ CommandsContext/Tags/sendColab.js
@@ -1,25 +1,34 @@
-const { ApplicationCommandType, ContextMenuCommandBuilder } = require('discord.js');
+const { ApplicationCommandType, ContextMenuCommandBuilder, EmbedBuilder } = require('discord.js');
 const { BotResponseBuilder } = require('../../utils');
 
 module.exports = {
-	category: 'Tags',
-	type: 'context-menu',
-	data: new ContextMenuCommandBuilder()
-		.setName('Send Colab links')
-		.setType(ApplicationCommandType.User),
-	async execute(interaction) {
-		const { client, targetUser } = interaction;
+    category: 'Tags',
+    type: 'context-menu',
+    data: new ContextMenuCommandBuilder()
+        .setName('Send Colab links')
+        .setType(ApplicationCommandType.User),
+    async execute(interaction) {
+        const { client, targetUser } = interaction;
 
-		if (targetUser.bot) return await interaction.reply({ content: 'That user is a bot.', ephemeral: true });
+        if (targetUser.bot) return await interaction.reply({ content: 'That user is a bot.', ephemeral: true });
 
-		const { botData, botConfigs } = client;
+        const { botData, botConfigs } = client;
 
-		const embeds = [...botData.embeds.colab.en];
-		embeds.pop();
+        const embeds = [];
 
-		const botResponse = new BotResponseBuilder();
-		botResponse.setText(`Suggestion for ${targetUser}`);
-		botResponse.addEmbeds(embeds, botConfigs);
-		await interaction.reply(botResponse.build());
-	},
+        const embedData = botData.embeds.colab.en;
+
+        embedData.embeds.forEach(content => {
+            const currentEmbed = new EmbedBuilder()
+                .setTitle(content.title)
+                .setDescription(content.description.join('\n'))
+                .setColor(content.color ?? 'Yellow');
+            embeds.push(currentEmbed);
+        });
+
+        const botResponse = { embeds: embeds };
+        botResponse.content = embedData.mentionMessage.replace('$user', targetUser);
+
+        await interaction.reply(botResponse);
+    },
 };
