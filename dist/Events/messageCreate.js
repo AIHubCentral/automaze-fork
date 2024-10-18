c5519e1500bc87e312dbafb038cb0f6d0f2a4c25
RayTracerGC <141577659+RayTracerGC@users.noreply.github.com>
2024-10-18 18:08:00 +0000

    remove link to repo when bot is mentioned

--- dist/Events/messageCreate.js
+++ dist/Events/messageCreate.js
@@ -5,102 +5,6 @@ const discordUtilities_1 = require("../Utils/discordUtilities");
 const nlpClassifier_1 = require("../Database/nlpClassifier");
 const botUtilities_1 = require("../Utils/botUtilities");
 const generalUtilities_1 = require("../Utils/generalUtilities");
-function handlePrefixCommand(prefix, message, client) {
-    const commandArguments = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = commandArguments.shift()?.toLowerCase();
-    if (!commandName)
-        return;
-    client.logger.debug('prefix command', {
-        input: `${prefix}${commandName}`,
-        args: commandArguments,
-    });
-    // Use the command alias if there's any, if there's none use the real command name instead
-    const command = client.commands.get(commandName) ||
-        client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
-    if (!command)
-        return;
-    client.logger.info('Executing prefix command', {
-        more: {
-            commandName: commandName,
-            guildId: message.guild?.id,
-            channelId: message.channel.id,
-            type: 'prefix',
-        },
-    });
-    command.run(client, message, commandArguments, prefix);
-}
-async function handleBotMentioned(prefix, message, client) {
-    const mention = '<@' + client.user?.id + '>';
-    if (!message.content.includes(mention))
-        return;
-    const embedData = {
-        title: "Wassup I'm Automaze!",
-        color: client.botConfigs.colors.theme.primary,
-        description: [],
-    };
-    embedData.description?.push(`\n- My prefix in this server is \`${prefix}\``);
-    embedData.description?.push("- Interested in how I'm built? [I'm actually open source!](https://github.com/DeprecatedTable/automaze)");
-    embedData.description?.push('- Forgot a specific command? Try `/help` or `-help`');
-    const embed = (0, discordUtilities_1.createEmbed)(embedData);
-    await message.reply({ embeds: [embed] });
-    const currentChannel = message.channel;
-    client.logger.info('Bot mentioned', {
-        more: {
-            guildId: message.guild?.id,
-            channelId: currentChannel.id,
-            channelName: currentChannel.name,
-        },
-    });
-}
-async function handleFaqQuestions(userId, message, repliedUsers, logger) {
-    const userInput = message.content.toLowerCase().trim();
-    const tokens = nlpClassifier_1.tokenizer.tokenize(userInput);
-    const keywords = (0, botUtilities_1.getFaqKeywords)();
-    const matchedKeyword = (0, botUtilities_1.containsKeyword)(tokens, keywords);
-    if (matchedKeyword && (0, botUtilities_1.containsQuestionPattern)(userInput)) {
-        // check if already replied to user
-        if (repliedUsers.has(userId))
-            return;
-        const messageChannel = message.channel;
-        let response = null;
-        switch (matchedKeyword) {
-            case 'epoch':
-            case 'epochs':
-                response =
-                    'Epoch is the number of iterations performed to complete one full cycle of the dataset during training. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
-                break;
-            case 'dataset':
-            case 'datasets':
-                response = `Datasets are a set of audio files compressed into a .zip file, used by RVC for voice training. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)`;
-                break;
-            case 'model':
-            case 'models':
-                response =
-                    'A model is the result of training on a dataset. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
-                break;
-            case 'inference':
-                response =
-                    'Inference is the process where an audio is transformed by the voice model. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
-                break;
-            case 'overtraining':
-                response =
-                    'A solid way to detect overtraining is checking if the **TensorBoard** graph starts rising and never comes back down, leading to robotic, muffled output with poor articulation. You can learn more about it in the [Applio Docs](https://docs.applio.org/getting-started/tensorboard)';
-                break;
-        }
-        if (response != null) {
-            messageChannel.sendTyping();
-            await (0, generalUtilities_1.delay)(50 * response.length);
-            await message.reply({ content: response, allowedMentions: { repliedUser: true } });
-            repliedUsers.set(userId, Date.now());
-            logger.info('Sent FAQ reply', {
-                guildId: messageChannel.guildId,
-                channelId: messageChannel.id,
-                channelName: messageChannel.name,
-                keyword: matchedKeyword,
-            });
-        }
-    }
-}
 const messageCreateEvent = {
     name: 'messageCreate',
     once: false,
@@ -173,3 +77,102 @@ const messageCreateEvent = {
     },
 };
 exports.default = messageCreateEvent;
+// helper methods
+function handlePrefixCommand(prefix, message, client) {
+    const commandArguments = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = commandArguments.shift()?.toLowerCase();
+    if (!commandName)
+        return;
+    client.logger.debug('prefix command', {
+        input: `${prefix}${commandName}`,
+        args: commandArguments,
+    });
+    // Use the command alias if there's any, if there's none use the real command name instead
+    const command = client.commands.get(commandName) ||
+        client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command)
+        return;
+    client.logger.info('Executing prefix command', {
+        more: {
+            commandName: commandName,
+            guildId: message.guild?.id,
+            channelId: message.channel.id,
+            type: 'prefix',
+        },
+    });
+    command.run(client, message, commandArguments, prefix);
+}
+async function handleBotMentioned(prefix, message, client) {
+    const mention = '<@' + client.user?.id + '>';
+    if (!message.content.includes(mention))
+        return;
+    const embedData = {
+        title: "Wassup I'm Automaze!",
+        color: client.botConfigs.colors.theme.primary,
+        description: [
+            `\n➡ My prefix in this server is ${(0, discord_js_1.inlineCode)(prefix)}`,
+            `➡ Forgot a specific command? Use ${(0, discord_js_1.inlineCode)('/help')}`,
+        ],
+    };
+    // legacy repo https://github.com/DeprecatedTable/automaze
+    const currentChannel = message.channel;
+    await currentChannel.sendTyping();
+    await (0, generalUtilities_1.delay)(2_000);
+    await message.reply({ embeds: [(0, discordUtilities_1.createEmbed)(embedData)], allowedMentions: { repliedUser: true } });
+    client.logger.info('Bot mentioned', {
+        more: {
+            guildId: message.guild?.id,
+            channelId: currentChannel.id,
+            channelName: currentChannel.name,
+        },
+    });
+}
+async function handleFaqQuestions(userId, message, repliedUsers, logger) {
+    const userInput = message.content.toLowerCase().trim();
+    const tokens = nlpClassifier_1.tokenizer.tokenize(userInput);
+    const keywords = (0, botUtilities_1.getFaqKeywords)();
+    const matchedKeyword = (0, botUtilities_1.containsKeyword)(tokens, keywords);
+    if (matchedKeyword && (0, botUtilities_1.containsQuestionPattern)(userInput)) {
+        // check if already replied to user
+        if (repliedUsers.has(userId))
+            return;
+        const messageChannel = message.channel;
+        let response = null;
+        switch (matchedKeyword) {
+            case 'epoch':
+            case 'epochs':
+                response =
+                    'Epoch is the number of iterations performed to complete one full cycle of the dataset during training. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
+                break;
+            case 'dataset':
+            case 'datasets':
+                response = `Datasets are a set of audio files compressed into a .zip file, used by RVC for voice training. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)`;
+                break;
+            case 'model':
+            case 'models':
+                response =
+                    'A model is the result of training on a dataset. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
+                break;
+            case 'inference':
+                response =
+                    'Inference is the process where an audio is transformed by the voice model. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
+                break;
+            case 'overtraining':
+                response =
+                    'A solid way to detect overtraining is checking if the **TensorBoard** graph starts rising and never comes back down, leading to robotic, muffled output with poor articulation. You can learn more about it in the [Applio Docs](https://docs.applio.org/getting-started/tensorboard)';
+                break;
+        }
+        if (response != null) {
+            messageChannel.sendTyping();
+            await (0, generalUtilities_1.delay)(50 * response.length);
+            await message.reply({ content: response, allowedMentions: { repliedUser: true } });
+            repliedUsers.set(userId, Date.now());
+            logger.info('Sent FAQ reply', {
+                guildId: messageChannel.guildId,
+                channelId: messageChannel.id,
+                channelName: messageChannel.name,
+                keyword: matchedKeyword,
+            });
+        }
+    }
+}
--- src/Events/messageCreate.ts
+++ src/Events/messageCreate.ts
@@ -24,126 +24,6 @@ import {
 import { delay } from '../Utils/generalUtilities';
 import winston from 'winston';
 
-function handlePrefixCommand(prefix: string, message: Message, client: ExtendedClient): void {
-    const commandArguments = message.content.slice(prefix.length).trim().split(/ +/);
-
-    const commandName = commandArguments.shift()?.toLowerCase();
-    if (!commandName) return;
-
-    client.logger.debug('prefix command', {
-        input: `${prefix}${commandName}`,
-        args: commandArguments,
-    });
-
-    // Use the command alias if there's any, if there's none use the real command name instead
-    const command =
-        client.commands.get(commandName) ||
-        client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
-    if (!command) return;
-
-    client.logger.info('Executing prefix command', {
-        more: {
-            commandName: commandName,
-            guildId: message.guild?.id,
-            channelId: message.channel.id,
-            type: 'prefix',
-        },
-    });
-
-    command.run(client, message, commandArguments, prefix);
-}
-
-async function handleBotMentioned(prefix: string, message: Message, client: ExtendedClient): Promise<void> {
-    const mention: string = '<@' + client.user?.id + '>';
-    if (!message.content.includes(mention)) return;
-
-    const embedData: EmbedData = {
-        title: "Wassup I'm Automaze!",
-        color: client.botConfigs.colors.theme.primary,
-        description: [],
-    };
-
-    embedData.description?.push(`\n- My prefix in this server is \`${prefix}\``);
-    embedData.description?.push(
-        "- Interested in how I'm built? [I'm actually open source!](https://github.com/DeprecatedTable/automaze)"
-    );
-    embedData.description?.push('- Forgot a specific command? Try `/help` or `-help`');
-
-    const embed = createEmbed(embedData);
-    await message.reply({ embeds: [embed] });
-
-    const currentChannel = message.channel as TextChannel;
-
-    client.logger.info('Bot mentioned', {
-        more: {
-            guildId: message.guild?.id,
-            channelId: currentChannel.id,
-            channelName: currentChannel.name,
-        },
-    });
-}
-
-async function handleFaqQuestions(
-    userId: string,
-    message: Message,
-    repliedUsers: Collection<string, number>,
-    logger: winston.Logger
-): Promise<void> {
-    const userInput = message.content.toLowerCase().trim();
-    const tokens = tokenizer.tokenize(userInput);
-    const keywords = getFaqKeywords();
-
-    const matchedKeyword = containsKeyword(tokens, keywords);
-
-    if (matchedKeyword && containsQuestionPattern(userInput)) {
-        // check if already replied to user
-        if (repliedUsers.has(userId)) return;
-
-        const messageChannel = message.channel as TextChannel;
-        let response = null;
-
-        switch (matchedKeyword) {
-            case 'epoch':
-            case 'epochs':
-                response =
-                    'Epoch is the number of iterations performed to complete one full cycle of the dataset during training. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
-                break;
-            case 'dataset':
-            case 'datasets':
-                response = `Datasets are a set of audio files compressed into a .zip file, used by RVC for voice training. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)`;
-                break;
-            case 'model':
-            case 'models':
-                response =
-                    'A model is the result of training on a dataset. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
-                break;
-            case 'inference':
-                response =
-                    'Inference is the process where an audio is transformed by the voice model. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
-                break;
-            case 'overtraining':
-                response =
-                    'A solid way to detect overtraining is checking if the **TensorBoard** graph starts rising and never comes back down, leading to robotic, muffled output with poor articulation. You can learn more about it in the [Applio Docs](https://docs.applio.org/getting-started/tensorboard)';
-                break;
-        }
-
-        if (response != null) {
-            messageChannel.sendTyping();
-            await delay(50 * response.length);
-            await message.reply({ content: response, allowedMentions: { repliedUser: true } });
-
-            repliedUsers.set(userId, Date.now());
-
-            logger.info('Sent FAQ reply', {
-                guildId: messageChannel.guildId,
-                channelId: messageChannel.id,
-                channelName: messageChannel.name,
-                keyword: matchedKeyword,
-            });
-        }
-    }
-}
-
 const messageCreateEvent: IEventData = {
     name: 'messageCreate',
     once: false,
@@ -232,3 +112,125 @@ const messageCreateEvent: IEventData = {
 };
 
 export default messageCreateEvent;
+
+// helper methods
+
+function handlePrefixCommand(prefix: string, message: Message, client: ExtendedClient): void {
+    const commandArguments = message.content.slice(prefix.length).trim().split(/ +/);
+
+    const commandName = commandArguments.shift()?.toLowerCase();
+    if (!commandName) return;
+
+    client.logger.debug('prefix command', {
+        input: `${prefix}${commandName}`,
+        args: commandArguments,
+    });
+
+    // Use the command alias if there's any, if there's none use the real command name instead
+    const command =
+        client.commands.get(commandName) ||
+        client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command) return;
+
+    client.logger.info('Executing prefix command', {
+        more: {
+            commandName: commandName,
+            guildId: message.guild?.id,
+            channelId: message.channel.id,
+            type: 'prefix',
+        },
+    });
+
+    command.run(client, message, commandArguments, prefix);
+}
+
+async function handleBotMentioned(prefix: string, message: Message, client: ExtendedClient): Promise<void> {
+    const mention: string = '<@' + client.user?.id + '>';
+    if (!message.content.includes(mention)) return;
+
+    const embedData: EmbedData = {
+        title: "Wassup I'm Automaze!",
+        color: client.botConfigs.colors.theme.primary,
+        description: [
+            `\n➡ My prefix in this server is ${inlineCode(prefix)}`,
+            `➡ Forgot a specific command? Use ${inlineCode('/help')}`,
+        ],
+    };
+
+    // legacy repo https://github.com/DeprecatedTable/automaze
+
+    const currentChannel = message.channel as TextChannel;
+
+    await currentChannel.sendTyping();
+    await delay(2_000);
+    await message.reply({ embeds: [createEmbed(embedData)], allowedMentions: { repliedUser: true } });
+
+    client.logger.info('Bot mentioned', {
+        more: {
+            guildId: message.guild?.id,
+            channelId: currentChannel.id,
+            channelName: currentChannel.name,
+        },
+    });
+}
+
+async function handleFaqQuestions(
+    userId: string,
+    message: Message,
+    repliedUsers: Collection<string, number>,
+    logger: winston.Logger
+): Promise<void> {
+    const userInput = message.content.toLowerCase().trim();
+    const tokens = tokenizer.tokenize(userInput);
+    const keywords = getFaqKeywords();
+
+    const matchedKeyword = containsKeyword(tokens, keywords);
+
+    if (matchedKeyword && containsQuestionPattern(userInput)) {
+        // check if already replied to user
+        if (repliedUsers.has(userId)) return;
+
+        const messageChannel = message.channel as TextChannel;
+        let response = null;
+
+        switch (matchedKeyword) {
+            case 'epoch':
+            case 'epochs':
+                response =
+                    'Epoch is the number of iterations performed to complete one full cycle of the dataset during training. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
+                break;
+            case 'dataset':
+            case 'datasets':
+                response = `Datasets are a set of audio files compressed into a .zip file, used by RVC for voice training. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)`;
+                break;
+            case 'model':
+            case 'models':
+                response =
+                    'A model is the result of training on a dataset. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
+                break;
+            case 'inference':
+                response =
+                    'Inference is the process where an audio is transformed by the voice model. You can learn more about it in the [Applio Docs](https://docs.applio.org/faq)';
+                break;
+            case 'overtraining':
+                response =
+                    'A solid way to detect overtraining is checking if the **TensorBoard** graph starts rising and never comes back down, leading to robotic, muffled output with poor articulation. You can learn more about it in the [Applio Docs](https://docs.applio.org/getting-started/tensorboard)';
+                break;
+        }
+
+        if (response != null) {
+            messageChannel.sendTyping();
+            await delay(50 * response.length);
+            await message.reply({ content: response, allowedMentions: { repliedUser: true } });
+
+            repliedUsers.set(userId, Date.now());
+
+            logger.info('Sent FAQ reply', {
+                guildId: messageChannel.guildId,
+                channelId: messageChannel.id,
+                channelName: messageChannel.name,
+                keyword: matchedKeyword,
+            });
+        }
+    }
+}
